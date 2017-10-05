60b471dcf78d6164866e5cd1e99ffb3057409c9e
Haochen Wang <wanghaochen56@gmail.com>
2017-10-05 19:26:04 +0800

    Revert "add function task"

--- app/src/main/java/com/wangh/e_university/ExamItem.java
+++ app/src/main/java/com/wangh/e_university/ExamItem.java
@@ -1,31 +1,29 @@
 package com.wangh.e_university;
 
 import org.jsoup.nodes.Element;
-import org.jsoup.nodes.Node;
 
 /**
  * Created by wangh on 2016/8/11.
  */
-public class ExamItem {
-    private String title;
+public class ExamItem extends BaseScheduleItem {
     private String teacher;
-    private String time;
-    private String location;
     private String modus;
+    private int startHour;
+    private int endHour;
+    private int startMinute;
+    private int endMinute;
     private int credit;
-    private int colorID;
 
     public ExamItem(){
-
+        setExam(true);
     }
 
     public ExamItem(String title, String teacher, String time, String location, String modus, int credit) {
-        this.title = title;
+        super(title, time.replaceAll("\\s", "\n"), location);
         this.teacher = teacher;
-        this.time = time.replaceAll("\\s","\n");
-        this.location = location;
         this.modus = modus;
         this.credit = credit;
+        getTimes();
     }
 
     public int getCredit() {
@@ -36,14 +34,6 @@ public class ExamItem {
         this.credit = credit;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getTeacher() {
         return teacher;
     }
@@ -52,56 +42,86 @@ public class ExamItem {
         this.teacher = teacher;
     }
 
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
     public String getModus() {
         return modus;
+    }
+
+    @Override
+    public void setTime(String time) {
+        super.setTime(time);
+        getTimes();
     }
 
     public void setModus(String modus) {
         this.modus = modus;
     }
 
-    public void setColorID(int colorID) {
-        this.colorID = colorID;
+    public int getStartHour() {
+        return startHour;
     }
 
-    public int getColorID() {
-        return colorID;
+    public void setStartHour(int startHour) {
+        this.startHour = startHour;
+    }
+
+    public int getEndHour() {
+        return endHour;
+    }
+
+    public void setEndHour(int endHour) {
+        this.endHour = endHour;
+    }
+
+    public int getStartMinute() {
+        return startMinute;
+    }
+
+    public void setStartMinute(int startMinute) {
+        this.startMinute = startMinute;
+    }
+
+    public int getEndMinute() {
+        return endMinute;
+    }
+
+    public void setEndMinute(int endMinute) {
+        this.endMinute = endMinute;
     }
 
     public void parseExam(Element exam){
-        title=exam.child(2).text();
+        setTitle(exam.child(2).text());
         teacher=exam.child(4).text();
         credit=Integer.parseInt(exam.child(5).text());
-        time=exam.child(6).text();
-        location=exam.child(7).text();
+        setTime(exam.child(6).text());
+        setLocation(exam.child(7).text());
         modus=exam.child(8).text();
+        getTimes();
+    }
+
+    private void getTimes(){
+        setMonth(Integer.parseInt(getTime().split("\\s")[0].split("-")[1]) - 1);
+        setDay(Integer.parseInt(getTime().split("\\s")[0].split("-")[2]));
+        startHour=(Integer.parseInt(getTime().split("\\s")[1].split("-")[0].split(":")[0]));
+        endHour=(Integer.parseInt(getTime().split("\\s")[1].split("-")[1].split(":")[0]));
+        startMinute=(Integer.parseInt(getTime().split("\\s")[1].split("-")[0].split(":")[1]));
+        endMinute=(Integer.parseInt(getTime().split("\\s")[1].split("-")[1].split(":")[1]));
     }
 
     @Override
     public String toString() {
         return "ExamItem{" +
-                "title='" + title + '\'' +
+                "title='" + getTitle() + '\'' +
                 ", teacher='" + teacher + '\'' +
-                ", time='" + time + '\'' +
-                ", location='" + location + '\'' +
+                ", time='" + getTime() + '\'' +
+                ", location='" + getLocation() + '\'' +
                 ", modus='" + modus + '\'' +
-                ", credit=" + credit +
+                ", credit=" + credit + '\'' +
+                ", month=" + getMonth() + '\'' +
+                ", day=" + getDay() + '\'' +
+                ", startHour=" + startHour + '\'' +
+                ", endHour=" + endHour + '\'' +
+                ", startMinute=" + startMinute + '\'' +
+                ", endMinute=" + endMinute + '\'' +
                 '}';
     }
 }
